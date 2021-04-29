cb6a6e6ccbdd3257e975038c2216872cabfe64ac
Julie Bien-Aime <julie.bienaime01298@gmail.com>
2021-04-29 13:12:47 -0400

    Update App.java
    Started mapping out game

--- GoblinTower/src/main/java/com/julieb/goblintower/App.java
+++ GoblinTower/src/main/java/com/julieb/goblintower/App.java
@@ -5,6 +5,8 @@
  */
 package com.julieb.goblintower;
 
+import java.util.Random;
+
 /**
  *
  * @author julie
@@ -12,8 +14,56 @@ package com.julieb.goblintower;
 public class App {
     public static void main(String args[]){
         
+        boolean play = true;
+        Random rand = new Random();
         
+        // intialize hero
+        Hero hero = new Hero();
+        
+        
+        while (play){
+            //level tracker 
+            int stepCount =0; 
+            int level =1; 
+            
+            //1. take step 
+            //2. potion shop 
+            
+             if(rand.nextInt(5) == 3){
+                 
+                //spawn goblin 
+                 Goblin goblin = new Goblin();
+                 
+                 //start fight 
+             }
+                 
+        
+        
+        
+        }
         
         
   }
+    
+   public static void fight (Hero hero, Goblin goblin){
+       boolean fight = true;
+       
+       while (fight){
+        
+           //if goblin defense < hero defense --> goblin health -= hero attack 
+           
+           //else if goblin defense > hero defense --> hero health -= goblin attack
+           
+           //else if goblin defense == hero health --> goblin health -= hero attack && hero health -= goblin attack
+           
+           
+           //if goblin health <= 0  -> hero gold +2, fight = false;
+           
+           //if hero health <= 0 --> fight = false; 
+       
+       
+       }
+   }
+
+
 }

cdac30de623555ef827a06e0f50db17c7055104d
Julie Bien-Aime <julie.bienaime01298@gmail.com>
2021-05-11 13:27:33 -0400

    fight() method created

--- GoblinTower/src/main/java/com/julieb/goblintower/App.java
+++ GoblinTower/src/main/java/com/julieb/goblintower/App.java
@@ -47,21 +47,36 @@ public class App {
     
    public static void fight (Hero hero, Goblin goblin){
        boolean fight = true;
+       int goblinHealth = goblin.getHealth();
+       int heroHealth = hero.getHealth();
        
        while (fight){
-        
+           
            //if goblin defense < hero defense --> goblin health -= hero attack 
+           if (goblin.getDefense() < hero.getDefense()){
+               goblinHealth -= hero.getPower();
            
+           }
            //else if goblin defense > hero defense --> hero health -= goblin attack
-           
+           else if(goblin.getDefense() > hero.getDefense()){
+              heroHealth -= goblin.getPower();
+           }
            //else if goblin defense == hero health --> goblin health -= hero attack && hero health -= goblin attack
-           
+           else if(goblin.getDefense() == hero.getDefense()){
+               goblinHealth -= hero.getPower();
+               heroHealth -= goblin.getPower();
+           }
            
            //if goblin health <= 0  -> hero gold +2, fight = false;
+           if(goblinHealth <= 0 ){
+               hero.setGold(hero.getGold() +2);
+               fight = false;
+           }
            
            //if hero health <= 0 --> fight = false; 
-       
-       
+           if(heroHealth <= 0){
+               fight = false;
+           }      
        }
    }
 
